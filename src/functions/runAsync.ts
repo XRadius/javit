5c9cc1550e640b1205657971a2350bb292775b04
Blastradius <>
2023-04-03 18:35:18 +0200

    Fix exceeding maximum filename length

--- src/functions/runAsync.ts
+++ src/functions/runAsync.ts
@@ -6,7 +6,7 @@ import sanitizeFilename from 'sanitize-filename';
 
 export async function runAsync(filePath: string, value: app.Metadata) {
   const {dir, ext} = path.parse(filePath);
-  const fileName = sanitizeFilename(value.title);
+  const fileName = sanitizeFilename(value.title).substring(0, 245).trim();
   const imagePath = path.join(dir, `${fileName}.jpg`);
   const videoPath = path.join(dir, `${fileName}${ext}`);
   await fileAsync(filePath, videoPath);
