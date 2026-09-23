a82f81d8553f2033aba8129c4b97879df837caf2
Blastradius <>
2026-09-23 17:12:03 +0200

    refactor: rename variables

--- src/functions/utils/getPosterAsync.js
+++ src/functions/utils/getPosterAsync.js
@@ -3,10 +3,10 @@ import sharp from "sharp";
 /** @param {Buffer} buffer */
 export async function getPosterAsync(buffer) {
   const image = sharp(buffer);
-  const imageData = await image.metadata();
-  const height = imageData.height;
-  const width = imageData.width * 0.475;
-  const left = imageData.width - width;
+  const metadata = await image.metadata();
+  const height = metadata.height;
+  const width = metadata.width * 0.475;
+  const left = metadata.width - width;
   const top = 0;
   return await image.extract({ height, left, top, width }).jpeg().toBuffer();
 }
